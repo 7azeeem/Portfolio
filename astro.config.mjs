82cc8f642f032f1669f6270271cc96225e20296a
Adel Ashraf <eng.adel.ashraf@gmail.com>
2026-07-08 14:53:27 +0300

    Add sitemap integration and update site URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
-import { defineConfig } from 'astro/config'
-import tailwind from "@astrojs/tailwind"
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
-import robotsTxt from "astro-robots-txt"
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
-})
+  site: "https://3del.vercel.app", 
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap(),
+  ],
+});
