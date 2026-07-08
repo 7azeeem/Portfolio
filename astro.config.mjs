1472d0748cbd09d91dbd92bc507cc9be6bbedc6e
Adel Ashraf <eng.adel.ashraf@gmail.com>
2026-07-08 15:07:39 +0300

    Remove sitemap integration from astro.config.mjs
    Removed sitemap integration from Astro configuration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://3del.vercel.app", 
   integrations: [
     tailwind(),
     robotsTxt(),
-    sitemap(),
   ],
 });
