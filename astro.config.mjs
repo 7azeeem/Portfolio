179bb8285d2fa17e9f0b1dc2d98bb970c69eb15f
Adel Ashraf <eng.adel.ashraf@gmail.com>
2026-07-24 16:00:41 +0300

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://3del.vercel.app", 
+  site: "https://3del.vercel.app",
   integrations: [
     tailwind(),
+    sitemap(),
     robotsTxt(),
   ],
 });
